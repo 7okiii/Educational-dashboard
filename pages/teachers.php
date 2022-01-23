e833ad866a0c1003d20b92ef663c54d3ace71cee
bloomingfloria <96505032+bloomingfloria@users.noreply.github.com>
2022-01-22 18:19:27 -0800

    Update teachers.php

--- pages/teachers.php
+++ pages/teachers.php
@@ -1,19 +1,48 @@
 <div>
     <h3>Mark page</h3>
-        <form action="<?php echo $_SERVER['PHP_SELF']."teachers"?>">
-            <label for="stuName">stuid</label>
-            <input type="text" name="stuName">
-            mark <input type="text">
-            <button type="submit">Chose</button>
-        </form>
-        <table>
-            <tr>
-                <th>Student name</th>
-                <th>Student id/th>
-                <th>subject</th>
-                <th>mark</th>
-            </tr>
-        </table>
+        <form action="<?php echo $_SERVER['PHP_SELF']."teachers"?>"
+  <label for="course">Course:</label><br>
+  <input type="text" id="course" name="course" value="Math"><br>
+  
+  <input type="submit" value="Submit">
+  </form>
+  
+  <style>
+table, th, td {
+  border:1px solid black;
+}
+</style>
+<h4>Course 1</h4>
+
+<table style="width:100%">
+  <tr>
+    <th>Student name</th>
+    <th>Marks</th>
+    <th>Comments</th>
+  </tr>
+  <tr>
+    <td>Alfreds </td>
+    <td>80</td>
+    <td>Dedicated and hard working student</td>
+  </tr>
+  <tr>
+    <td>Benzeo</td>
+    <td>90</td>
+    <td>Have great understanding in course</td>
+  </tr>
+</table>
+
+<p>Course marks and comments for students.</p>
+
+
+
+<?php
+//each teacher has different above form
+//depending on value, directs to different table in db
+
+
+?>
+
     </div>
 
 
