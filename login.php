d8d02b82f2665a0bbc8c349fbb8987282d72de12
Nnookk <naokiogata@NaokinoAir.com>
2022-01-22 23:43:41 -0500

    able to redirect to dashboard, need more function

--- login.php
+++ login.php
@@ -17,36 +17,54 @@
 </head>
 <body>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF']?>" enctype="multipart/form-data">
-        <label for="username">Username</label>
-        <input type="text" name="username" required>
+        <label for="userId">User ID</label>
+        <input type="text" name="userId" required>
         <label for="pass">Password</label>
         <input type="password" name="pass" required>
         <button type="submit">Login</button>
     </form>  
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $dbCon = new mysqli($dbsvname,$dbusername,$dbpassword,$db_table);
+        //
+        $user_id = $_POST['userId'];
+        $pass = $_POST['pass'];
+        //connect with database 
+        $dbCon = new mysqli("localhost","root","","education");
+        $select_cmd = "SELECT salt FROM users_tb WHERE user_id ='".$user_id."'";
+        $result = $dbCon->query($select_cmd);
         if ($dbCon->connect_error) {
             echo "<p style='color:red;'>Connection error</p>";
         } else {
-            echo "<p style='color:green;'>Connected to the database</p>";
+            // echo "<p style='color:green;'>Connected to the database</p>";
             // take the data from database and check the type of users
-            $fname = $_POST['username'];
-            $select_cmd = "SELECT user_type FROM users_tb WHERE fname='".$fname."'";
-            $result = $dbCon->query($select_cmd);
+            // $select_cmd = "SELECT * FROM users_tb WHERE user_id='".$user_id."'";
+            // if ($user_id == $row['user_id'] && $pass)
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    $userType = $row['user_type'];
-                    if ($userType == 'admin') {
-                        echo "admin";
-                    } elseif ($userType == 'teacher') {
-                        echo "teacher";
-                    } else {
-                        echo "student";
-                    }
+                    $salt = $row['salt'];
                 }    
+                $tmpPass = md5($pass.$salt);
+                echo $tmpPass;// working
+                $select_cmd = "SELECT * FROM users_tb WHERE user_id='".$user_id."' AND password='".$tmpPass."'";
+                $result = $dbCon->query($select_cmd);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo $row['fname']." is logged in";
+                        header("Location: dashboard.php");
+                        exit();
+                    }
+                }
+                // $userType = $row['user_type'];
+                // if ($userType == 'admin') {
+                //     echo "admin";
+                // } elseif ($userType == 'teacher') {
+                //     echo "teacher";
+                // } else {
+                //     echo "student";
+                // }
+
             } else {
-                echo "<p style='color:red;'>Username or password is wrong</p>";
+                echo "<p style='color:red;'>User ID or password is wrong</p>";
             }
         }
     }
