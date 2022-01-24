d40a53f40fb2a73011246473c1a54efa6c5558e3
Nnookk <naokiogata@NaokinoAir.com>
2022-01-24 12:27:07 -0500

    you can use test2.css and change the name

--- pages/edit_admin_form.php
+++ pages/edit_admin_form.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        <?php
+            include('../stylesheet/test2.css');
+        ?>
+    </style>
 
-    <?php
+</head>
+<body>
+<?php
         // if($_SERVER["REQUEST_METHOD"]=="GET"){
         //     $_SESSION["formdisplay"]=1; 
         //     $con=connect_to_database();
@@ -131,3 +145,5 @@
                 
                 <input id="submiting" type="submit" name="edit" value="Edit">
         </form>
+</body>
+</html>
