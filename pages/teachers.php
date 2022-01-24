905a98a0f8500a99b732ac0887397b365e4f065e
Nnookk <naokiogata@NaokinoAir.com>
2022-01-24 17:56:00 -0500

    good

--- pages/teachers.php
+++ pages/teachers.php
@@ -10,7 +10,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-    /* * {
+    * {
       box-sizing: border-box;
       margin: 0;
       padding: 0;
@@ -33,7 +33,7 @@
       width: 100%;
       height: 100%;
       text-align: center;
-    } */
+    }
   </style>
   <title>Teachers</title>
 </head>
